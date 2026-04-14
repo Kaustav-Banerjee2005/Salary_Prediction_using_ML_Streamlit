5561a878b083a539120f33383d3fe4b07abcbb93
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-14 13:54:50 +0530

    corrected the syntax error in explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -31,7 +31,7 @@ import os
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("df = pd.read_csv("https://raw.githubusercontent.com/KevinLiao159/StackOverflowSurvey/master/data/survey_results_public.csv")")
+    df = pd.read_csv("https://raw.githubusercontent.com/KevinLiao159/StackOverflowSurvey/master/data/survey_results_public.csv")
     
     print("Available columns:", df.columns.tolist())  # Add this to see all columns
     
